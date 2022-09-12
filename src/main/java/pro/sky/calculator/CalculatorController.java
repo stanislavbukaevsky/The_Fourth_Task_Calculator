5f5d4079c9f6c536ca8974d719be469e2ad8d22b
stanislavi18061989 <96617178+stanislavbukaevsky@users.noreply.github.com>
2022-09-12 17:07:12 +0300

    Третий коммит

--- src/main/java/pro/sky/calculator/CalculatorController.java
+++ src/main/java/pro/sky/calculator/CalculatorController.java
@@ -21,21 +21,35 @@ public class CalculatorController {
 
     @GetMapping(path = "/plus")
     public String additionCalculator(@RequestParam(value = "num1", required = false) Integer numberOne, @RequestParam(value = "num2", required = false) Integer numberTwo) {
+        if (numberOne == null || numberTwo == null) {
+            return "Ошибка! Один из параметров пустой!";
+        }
         return calculatorService.additionCalculator(numberOne, numberTwo);
     }
 
     @GetMapping(path = "/minus")
     public String subtractionCalculator(@RequestParam(value = "num1", required = false) Integer numberOne, @RequestParam(value = "num2", required = false) Integer numberTwo) {
+        if (numberOne == null || numberTwo == null) {
+            return "Ошибка! Один из параметров пустой!";
+        }
         return calculatorService.subtractionCalculator(numberOne, numberTwo);
     }
 
     @GetMapping(path = "/multiply")
     public String multiplicationCalculator(@RequestParam(value = "num1", required = false) Integer numberOne, @RequestParam(value = "num2", required = false) Integer numberTwo) {
+        if (numberOne == null || numberTwo == null) {
+            return "Ошибка! Один из параметров пустой!";
+        }
         return calculatorService.multiplicationCalculator(numberOne, numberTwo);
     }
 
     @GetMapping(path = "/divide")
     public String divisionCalculator(@RequestParam(value = "num1", required = false) Integer numberOne, @RequestParam(value = "num2", required = false) Integer numberTwo) {
+        if (numberOne == null || numberTwo == null) {
+            return "Ошибка! Один из параметров пустой!";
+        } else if (numberOne == 0 || numberTwo == 0) {
+            return "Ошибка! Деление на ноль запрещено!";
+        }
         return calculatorService.divisionCalculator(numberOne, numberTwo);
     }
 }
--- src/main/java/pro/sky/calculator/CalculatorServiceImpl.java
+++ src/main/java/pro/sky/calculator/CalculatorServiceImpl.java
@@ -10,39 +10,21 @@ public class CalculatorServiceImpl implements CalculatorService {
 
     public String additionCalculator(Integer numberOne, Integer numberTwo) {
         int result = numberOne + numberTwo;
-        if (numberOne == null || numberTwo == null) {
-            return "Ошибка! Один из параметров пустой!";
-        } else {
             return "Результат сложения " + numberOne + " и " + numberTwo + " равен " + result;
-        }
     }
 
     public String subtractionCalculator(Integer numberOne, Integer numberTwo) {
         int result = numberOne - numberTwo;
-        if (numberOne == null || numberTwo == null) {
-            return "Ошибка! Один из параметров пустой!";
-        } else {
             return "Результат вычитания " + numberOne + " и " + numberTwo + " равен " + result;
-        }
     }
 
     public String multiplicationCalculator(Integer numberOne, Integer numberTwo) {
         int result = numberOne * numberTwo;
-        if (numberOne == null || numberTwo == null) {
-            return "Ошибка! Один из параметров пустой!";
-        } else {
             return "Результат умножения " + numberOne + " и " + numberTwo + " равен " + result;
-        }
     }
 
     public String divisionCalculator(Integer numberOne, Integer numberTwo) {
-        int result = numberOne / numberTwo;
-        if (numberOne == 0 || numberTwo == 0) {
-            return "Ошибка! Деление на ноль запрещено!";
-        } else if (numberOne == null || numberTwo == null) {
-            return "Ошибка! Один из параметров пустой!";
-        } else {
-            return "Результат деления " + numberOne + " и " + numberTwo + " равен " + result;
-        }
+        Integer result = numberOne / numberTwo;
+        return "Результат деления " + numberOne + " и " + numberTwo + " равен " + result;
     }
 }
